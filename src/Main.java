65fd8f4b19a3edc6bd94f0f993f6b67da4f56f61
Anna-Filipova <62184331+Anna-Filipova@users.noreply.github.com>
2020-04-05 20:43:26 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
-
-    long bonusBelowLimitForRegistered = service.calculate(1000_60, true);
-    System.out.println(bonusBelowLimitForRegistered);
-
-    long bonusOverLimitForRegistered = service.calculate(1_000_000_60, true);
-    System.out.println(bonusOverLimitForRegistered);
-
-    long bonusBelowLimitForUnRegistered = service.calculate(1000_60, false);
-    System.out.println(bonusBelowLimitForUnRegistered);
-
-    long bonusOverLimitForUnRegistered = service.calculate(1_000_000_60, true);
-    System.out.println(bonusOverLimitForUnRegistered);
-  }
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
 }
 
