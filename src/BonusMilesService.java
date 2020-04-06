a56b5cab468eac9f7d8a30d0bccb2d6424e93218
Anna-Filipova <62184331+Anna-Filipova@users.noreply.github.com>
2020-04-07 02:35:33 +0300

    Update and rename BonusService.java to BonusMilesService.java

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+  public long calculate(int cost) {
+   int cost = 9_800;
+    int accruals = 20;
+    long miles = cost / accruals;
+    System.out.println(miles);
+    }
+    return miles;
+  }
+}
+
+
+
